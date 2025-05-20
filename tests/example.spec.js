c6921f739917821fe6e82448438a1c152646211f
Sameer Shaik <sameer.kattubadi@gmail.com>
2025-05-20 09:31:09 +0530

    project init

---
+++ tests/example.spec.js
@@ -0,0 +1,8 @@
+// @ts-check
+import { test, expect } from "@playwright/test";
+
+test(`Test Name`, function () {
+  const number1 = 2;
+  const number2 = 3;
+  expect(number1 + number2).toBe(5);
+});
