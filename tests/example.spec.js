58c6a6d0cac56f3594d5eea5f384f16f895304dc
Sameer Shaik <sameer.kattubadi@gmail.com>
2025-05-21 23:35:25 +0530

    number test case

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,8 +1,13 @@
 // @ts-check
 import { test, expect } from "@playwright/test";
+import { addTwoNumbers, multiplyTwoNumbers } from "..";
 
-test(`Test Name`, function () {
-  const number1 = 2;
-  const number2 = 3;
-  expect(number1 + number2).toBe(5);
+test(`Add Two numbers Test case`, () => {
+  const result = addTwoNumbers(10, 20);
+  expect(result).toBe(30);
+});
+
+test(`Multiply Two numbers Test case`, () => {
+  const result = multiplyTwoNumbers(10, 20);
+  expect(result).toBe(200);
 });
